91ce8878908e5e783dc017781f887a3a55cafa1a
Ivan Kovalisko <ivankovalisko@Valerii-Shypunovs-MacBook-Pro-2.local>
2011-11-19 20:44:28 +0200

    Initial Commit

---
+++ ServerWatcher/RootViewController.h
@@ -0,0 +1,20 @@
+//
+//  RootViewController.h
+//  ServerWatcher
+//
+//  Created by Ivan Kovalisko on 19.11.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+
+}
+
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+
+@end
